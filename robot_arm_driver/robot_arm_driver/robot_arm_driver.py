95cd61ddba8da2ca7e8c3aa87d7a2b8fffc88ff8
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-11-09 11:42:07 +0800

    added torque calculation

--- robot_arm_driver/robot_arm_driver/robot_arm_driver.py
+++ robot_arm_driver/robot_arm_driver/robot_arm_driver.py
@@ -16,7 +16,10 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
-
+from control_msgs.msg import JointTrajectoryControllerState
+from std_msgs.msg import Bool
+from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy
+from rclpy.qos import QoSProfile
 from pyfeetech.servo import Revolute, Prismatic, Controller
 
 revolute_params = [
@@ -29,6 +32,8 @@ revolute_params = [
     'step_per_sec',
     'speed',
     'acceleration',
+    'torque',
+    'max_torque',
     'offset',
     'offset_gain',
     'horn_radius'
@@ -45,6 +50,7 @@ class RobotArmDriver(Node):
             parameters=[
                 ('serial_port', '/dev/ttyUSB0'),
                 ('baud_rate', 1000000),
+                ('initial_pos', [0,0,0,0,0,0]),
                 ('revolute_joint_names', []),
                 ('prismatic_joint_names', []),
         ])
@@ -61,7 +67,7 @@ class RobotArmDriver(Node):
             self._controller, 
             self._get_joints_config(rev_joint_names, 'revolute_joints')
         )
-        revolute_joints_ok = self._revolute_joints.init(disable_joints=True)
+        revolute_joints_ok = self._revolute_joints.init()
         if not revolute_joints_ok:
             print("Failed to init revolute joints.")
 
@@ -69,12 +75,14 @@ class RobotArmDriver(Node):
             self._controller, 
             self._get_joints_config(pris_joint_names, 'prismatic_joints')
         )
-        prismatic_joints_ok = self._prismatic_joints.init(disable_joints=True)
+        prismatic_joints_ok = self._prismatic_joints.init()
         if not prismatic_joints_ok:
             print("Failed to init prismatic joints.")
         
         self._joint_names = rev_joint_names + pris_joint_names
         self._joint_states = (len(rev_joint_names) + len(pris_joint_names)) * [0.0]
+        self._joint_goals = self.get_parameter('initial_pos').value
+        self._gripper_goal = 0        
 
         self._joint_states_msg = JointState()
         self._joint_states_msg.name = self._joint_names 
@@ -83,6 +91,33 @@ class RobotArmDriver(Node):
         self._joint_states_publisher = self.create_publisher(JointState, 'joint_states', 10)
 
         brake_timer = self.create_timer(0.02, self.control_callback)
+
+        pos_goal_qos = QoSProfile(
+          durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+          reliability=QoSReliabilityPolicy.RELIABLE,
+          history=QoSHistoryPolicy.KEEP_LAST,
+          depth=1
+        )
+
+        self._pos_goal_subscirber = self.create_subscription(
+            JointTrajectoryControllerState,
+            'robot_arm_controller/state',
+            self._pos_goal_callback,
+            pos_goal_qos
+        )
+
+        self._pos_goal_subscirber = self.create_subscription(
+            Bool,
+            'gripper',
+            self._gripper_callback,
+            pos_goal_qos
+        )
+
+    def _gripper_callback(self, msg):
+        self._gripper_goal = msg.data
+
+    def _pos_goal_callback(self, msg):
+        self._joint_goals = msg.actual.positions
 
     def _get_joints_config(self, joint_names, joint_type):
         joints_config = {}
@@ -97,6 +132,14 @@ class RobotArmDriver(Node):
     def control_callback(self):
         rev_pos, rev_vel = self._revolute_joints.state
         pris_pos, pris_vel = self._prismatic_joints.state
+
+        self._revolute_joints.go_to(self._joint_goals)
+        if self._gripper_goal:
+            self._prismatic_joints.go_to([0.0])
+        else:
+            self._prismatic_joints.go_to([0.025])
+
+
         self._joint_states_msg.position = rev_pos + pris_pos
         self._joint_states_msg.velocity = rev_vel + rev_vel
 
