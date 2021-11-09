2663aab0b72a0e6c8afd1142fbd775527b7a32f5
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-11-09 18:29:32 +0800

    added moveit interface header files

--- robot_arm_server/include/robot_arm_server_node.h
+++ robot_arm_server/include/robot_arm_server_node.h
@@ -6,6 +6,15 @@
 #include <chrono>
 #include <cstdlib>
 #include <memory>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+#include <moveit_msgs/msg/attached_collision_object.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
+
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
