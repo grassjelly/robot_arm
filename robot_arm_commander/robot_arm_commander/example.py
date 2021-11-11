8687f3b8f6aca8da7008d76672437f807947af3f
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-11-12 03:50:34 +0800

    improved exiting

--- robot_arm_commander/robot_arm_commander/example.py
+++ robot_arm_commander/robot_arm_commander/example.py
@@ -1,51 +1,4 @@
 # # #!/usr/bin/env python3
-# import time
-# import threading
-# import rclpy
-# from robot_arm_commander.move_group_interface import MoveGroupInterface
-# from robot_arm_commander.gripper import Gripper
-
-# from geometry_msgs.msg import PoseStamped, Pose
-
-
-# def main(args=None):
-#     rclpy.init(args=args)
-
-#     manipulator = MoveGroupInterface("robot_arm", "base_mount", "tool_link")
-#     gripper = Gripper()
-#     executor = rclpy.executors.MultiThreadedExecutor(2)
-#     executor.add_node(manipulator)
-#     executor.add_node(gripper)
-#     thread = threading.Thread(target=executor.spin)
-#     thread.start()
-    
-#     joint_names = ['pan_joint', 'shoulder_joint', 'elbow_joint', 'wrist0_joint', 'wrist1_joint', 'wrist2_joint']
-#     while(1): 
-#         gripper.set_pos(0.0225)
-
-#         pose_msg = PoseStamped()
-#         pose_msg.pose.orientation.x = -1.155227619165089e-05
-#         pose_msg.pose.orientation.y = 0.46281740069389343
-#         pose_msg.pose.orientation.z = -1.7791414848034037e-06
-#         pose_msg.pose.orientation.w = 0.8864536285400391
-#         pose_msg.pose.position.x = 0.23239827156066895
-#         pose_msg.pose.position.y = 0.034903883934020996
-#         pose_msg.pose.position.z = 0.5444427728652954
-#         manipulator.move_to_pose(pose_msg)
-
-#         gripper.open()
-
-#         joint_pos = [0.0] * 6
-#         manipulator.move_to_joint_position(joint_names, joint_pos)
-
-#         gripper.close()
-#         time.sleep(5)
-
-#     rclpy.shutdown()
-
-# if __name__ == "__main__":
-#     main()
-# #!/usr/bin/env python3
 import time
 import threading
 import rclpy
@@ -63,7 +16,7 @@ def main(args=None):
     executor = rclpy.executors.MultiThreadedExecutor(2)
     executor.add_node(manipulator)
     executor.add_node(gripper)
-    thread = threading.Thread(target=executor.spin)
+    thread = threading.Thread(target=executor.spin, daemon=True)
     thread.start()
     
     joint_names = ['pan_joint', 'shoulder_joint', 'elbow_joint', 'wrist0_joint', 'wrist1_joint', 'wrist2_joint']
@@ -101,8 +54,6 @@ def main(args=None):
 
         joint_pos = [0.0] * 6
         manipulator.move_to_joint_position(joint_names, joint_pos)
-
-        time.sleep(5)
 
     rclpy.shutdown()
 
