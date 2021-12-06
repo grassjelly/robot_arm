7c001df8f3a617835b951cac11660811bfcb8788
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-12-07 01:34:37 +0800

    working pnp

--- robot_arm_apps/robot_arm_apps/pick_n_place.py
+++ robot_arm_apps/robot_arm_apps/pick_n_place.py
@@ -14,53 +14,58 @@ def main(args=None):
     manipulator = MoveGroupInterface(
         "robot_arm", 
         "base_mount", 
-        "tool_link",
-        offsets=(-0.015, 0.015, -0.4)
+        "tool_link"
     )
-    # gripper = Gripper()
+    gripper = Gripper()
     perception_client = PerceptionClient()
 
     executor = rclpy.executors.MultiThreadedExecutor(3)
     executor.add_node(manipulator)
-    # executor.add_node(gripper)
+    executor.add_node(gripper)
     executor.add_node(perception_client)
 
     thread = threading.Thread(target=executor.spin, daemon=True)
     thread.start()
 
-    # gripper.open()
     perception_client.send_request()
     joint_names = ['pan_joint', 'shoulder_joint', 'elbow_joint', 'wrist0_joint', 'wrist1_joint', 'wrist2_joint']
-
+    drop_pos = [-0.9249904150950417, 0.4049709280018093, 1.0799224746714913, 0.006135923151542565, 1.6352235198860934, 0.5767767762450011]
+    home_pose = [-1.533980787885641, -0.8513593372765309, 2.389942067525829, 0.01227184630308513, 1.5984079809768381, -0.01687378866674205]
+                
+    gripper.open()
+    manipulator.move_to_joint_position(joint_names, home_pose)
     while(1): 
         poses = perception_client.get_poses()
         if poses:
             for pose in poses:
                 ps = PoseStamped()
                 ps.pose = pose
+                ps.pose.position.x -= 0.01
+                ps.pose.position.y += 0.01
+                ps.pose.position.z = 0.01
 
-                ps.pose.position.z += 0.05
+                ps.pose.position.z += 0.10
                 manipulator.move_to_pose(ps)
                 time.sleep(1)
 
-                ps.pose.position.z -= 0.05
+                ps.pose.position.z -= 0.10
                 manipulator.move_to_pose(ps)
                 time.sleep(1)
-                # gripper.close()
+                gripper.close()
 
-                ps.pose.position.z += 0.05
+                ps.pose.position.z += 0.10
                 manipulator.move_to_pose(ps)
                 time.sleep(1)
 
-
-                joint_pos = [0.0] * 6
-                manipulator.move_to_joint_position(joint_names, joint_pos)
+                manipulator.move_to_joint_position(joint_names, drop_pos)
+                gripper.open()
             break
         else:
             pass
 
         rclpy.spin_once(perception_client)
 
+    manipulator.move_to_joint_position(joint_names, home_pose)
     rclpy.shutdown()
 
 
